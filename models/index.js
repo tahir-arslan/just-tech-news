60cea22027a564729897ec9f3a92e65cd7cd5534
ThisOneGuy <tahir.arslan@gmail.com>
2022-09-28 14:05:26 -0400

    finished 13.3

--- models/index.js
+++ models/index.js
@@ -1,3 +1,10 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+// create association from post to user, creating a one to many relation. post can only belong
+// to one user, not many (constraint defined below)
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+module.exports = { User, Post };
